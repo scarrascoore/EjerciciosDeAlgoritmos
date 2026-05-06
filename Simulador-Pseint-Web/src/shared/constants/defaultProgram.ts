5f4b17adf00dfc2be1a1084a533dea794b1462f3
scarrascoore <scarrascoore@gmail.com>
2026-05-06 14:13:41 -0500

    Crear el pseudocódigo inicial de ejemplo

---
+++ Simulador-Pseint-Web/src/shared/constants/defaultProgram.ts
@@ -0,0 +1,11 @@
+export const defaultProgram = `Algoritmo MiPrimerPrograma
+  Escribir "¡Hola! Bienvenido al editor de PSeInt."
+  Escribir "¿Cómo te llamas?"
+  Leer nombre
+  Escribir "Mucho gusto, ", nombre
+
+  Escribir "Ingresa un número para calcular su doble:"
+  Leer num
+  doble <- num * 2
+  Escribir "El doble de ", num, " es ", doble
+FinAlgoritmo`;
