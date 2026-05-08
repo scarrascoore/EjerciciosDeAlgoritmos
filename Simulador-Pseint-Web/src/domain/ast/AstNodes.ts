68ac06687785b65b032fc7b97d5a1ac10660073b
scarrascoore <scarrascoore@gmail.com>
2026-05-08 14:38:50 -0500

    Modificando para ampliar el AST

--- Simulador-Pseint-Web/src/domain/ast/AstNodes.ts
+++ Simulador-Pseint-Web/src/domain/ast/AstNodes.ts
@@ -1,29 +1,38 @@
 export interface ProgramNode {
-    type: "program";
-    name: string;
-    body: StatementNode[];
+  type: "program";
+  name: string;
+  body: StatementNode[];
 }
 
-export type StatementNode = 
-    |WriteStatementNode
-    | ReadStatementNode
-    | AssignmentStatementNode;
+export type StatementNode =
+  | WriteStatementNode
+  | ReadStatementNode
+  | AssignmentStatementNode
+  | IfStatementNode;
 
 export interface WriteStatementNode {
-    type: "write";
-    args: string[];
-    line: number;
+  type: "write";
+  args: string[];
+  line: number;
 }
 
 export interface ReadStatementNode {
-    type: "read";
-    variable: string;
-    line: number;
+  type: "read";
+  variable: string;
+  line: number;
 }
 
 export interface AssignmentStatementNode {
-    type: "assign";
-    variable: string;
-    expression: string;
-    line: number;
+  type: "assign";
+  variable: string;
+  expression: string;
+  line: number;
+}
+
+export interface IfStatementNode {
+  type: "if";
+  condition: string;
+  thenBranch: StatementNode[];
+  elseBranch: StatementNode[];
+  line: number;
 }
